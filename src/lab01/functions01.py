aa69d49038e7fb0995af4b5d00834447010a3fc1
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-10-02 15:10:08 -0600

    moved my source code into my lab01 folder

--- src/lab01/functions01.py
+++ src/lab01/functions01.py
@@ -11,6 +11,16 @@ import numpy as np
     # and the max allowable launch angle when the max altitude is (1-tol_alpha)*alpha)
 # use equation 18 to compute the inverse sine so that 5 sig digs are correct.
 
+
+def arcsin(x):
+    pass
+    # if -1 > x > 1, raise error!
+
+
+def launch_angle(y):
+    pass
+
+
 def launch_angle_range(ve_v0,alpha,tol_alpha):
     """
     Description of function:
@@ -29,15 +39,20 @@ def launch_angle_range(ve_v0,alpha,tol_alpha):
 
     #...
     # implementation of equations 17
-
+    sinphi = ((1 + alpha) * (np.sqrt(1 - (alpha / (1 + alpha)) * (ve_v0)**2)))  # eq 17 step 1
+    phi = np.arcsin(sinphi)
+    phi_range = [phi]
+    print(phi_range)
     # and 18
-
+    #sinarc = np.arcsin
+    #LHS_sin = np.square(arcsin)
     #...
 
     return phi_range
 
+"""
 sinphi = (1 + alpha) * (np.sqrt(1 - (alpha / (1 + alpha)) * (ve_v0) ^ 2))   # eq 17 step 1
 phi = np.arcsin(sinphi)     # eq 17 step 2
 
 sinarc = np.arcsin
-LHS_sin = np.square(arcsin)
+LHS_sin = np.square(arcsin)"""
