3c87540393e0f6bfb252a98b1be6a7ce2df030c2
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-10-12 16:27:37 -0600

    realized for part 3 of lab we need 2 graphs to compare ve_v0 and lpha with launch angles

--- examples/driver.py
+++ examples/driver.py
@@ -27,10 +27,39 @@ def main():
     alpha = 0.25  # Please input a value.
     tol_alpha = 0.04   # Please input a value.
 
-    # The Numpy linespace command creates an array with a specified amount of elements in a specific range.
-    alpha_range = np.linspace(0, 0.1, 100)
+    # We need 2 graphs, one for x as alpha and one for x as ve_v0 to compare each to the launch angle.
+    # First we will make x = ve_v0.
 
-    # Now we create two empty lists. One will store the minimum angles, and the other the maximum.
+    # The Numpy linespace command creates an array with a specified amount of elements in a specific range.
+    ve_v0_range = np.linspace(1.4, 2, 100)
+
+    # Now we create two empty lists. One will store the minimum ve_v0, and the other the maximum.
+    min_ve_v0 = []
+    max_ve_v0 = []
+
+    for ve_v0 in ve_v0_range:
+        phi_range = launch_angle_range(ve_v0, alpha, tol_alpha)
+        min_ve_v0.append(phi_range[1])  # This will append the minimum value.
+        max_ve_v0.append(phi_range[0])  # This will append the maximum value.
+
+    # Using the information above, we will create the plot.
+    plt.xlabel("Ve/V0 ratio")
+    plt.ylabel("Launch angle (radians)")
+    # Plotting the minimum ve_v0.
+    plt.plot(ve_v0_range, min_ve_v0, label="Minimum Ve/V0 ratio", linewidth=2, color="#495867")
+    # Plotting the maximum ve_v0.
+    plt.plot(ve_v0_range, max_ve_v0, label="Maximum Ve/V0 ratio", linewidth=2, color="#c18c5d")
+    plt.legend(title="Legend", alignment="left")
+    plt.title("Launch Angle as a Function of the Ratio of Escape Velocity to Terminal Velocity")
+    plt.savefig('C:\\Users\\sydne\\git\\goph419\\goph419-f2024-lab01-stSP\\figures\\launch_angle_vs_ve_v0.png')
+    plt.show()
+
+    # Now we will make x = alpha.
+
+    # The recommended values are 0.01-0.04.
+    alpha_range = np.linspace(0.01, 0.04, 100)
+
+    # Again, we create two empty lists. One will store the minimum angles, and the other the maximum.
     min_angles = []
     max_angles = []
 
