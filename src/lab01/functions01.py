e696cdedef6382117cadf02b681e057b4db13318
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-10-12 12:44:05 -0600

    Edited the description of the function as well as all the comments

--- src/lab01/functions01.py
+++ src/lab01/functions01.py
@@ -3,44 +3,34 @@ import numpy as np
 import math
 
 
-# STEP 1 #
-# make a function that
-    # takes an input ratio of escape velocity:terminal velocity (ve_v0),
-    # the desired max altitude as a fraction of Earth's radius (alpha)
-    # and the tolerance for max altitude (tol_alpha)
-# this function (launch_angle_range) should output a two component object (numpy.array) that contains
-    # the min allowable launch angle when the max altitude is (1+tol_alpha)*alpha
-    # and the max allowable launch angle when the max altitude is (1-tol_alpha)*alpha
-# use equation 18 to compute the inverse sine so that 5 sig digs are correct.
-
-
 def arcsin(x):
     """
     Description of function:
     -----
+    This function computes the inverse sine function.
 
     Parameter(s):
     -----
-        x:
+        x: value that is put in the inverse sine.
 
     Return(s):
     -----
-        result:
+        result: the inverse sine of x.
     """
     if x < -1 or x > 1:
         raise ValueError(f"You have an invalid x value: {x}")
-    # eq 18
     result = 0.0
     eps_a = 1.0
-    tol = 1.e-8  # error tolerance
+    tol = 1.e-8  # The significant digit tolerance. The general tolerance is 1.0e-8.
     n = 1
     fact_n = math.factorial(n)
     fact_2n = math.factorial(n*2)
     n_max = 100
     while eps_a > tol and n < n_max:
+        # The implementation of equation 18 in the lab manual.
         dy = ((2 * x) ** (2 * n)) / ((n**2) * (fact_2n/(fact_n**2)))
         result += dy
-        eps_a = abs(dy / result)  # error calc
+        eps_a = abs(dy / result)  # This is the relative error calculation.
         n += 1
         fact_n *= n
         fact_2n *= (2 * n) * ((2 * n) - 1)
@@ -51,6 +41,7 @@ def launch_angle(ve_v0, alpha):
     """
     Description of function:
     -----
+    This function takes an input ratio of escape velocity to terminal velocity and the desired maximum altitude as a fraction of Earth's radius, and uses equation 17 to compute the launch angle.
 
     Parameter(s):
     -----
@@ -59,12 +50,14 @@ def launch_angle(ve_v0, alpha):
 
     Return(s):
     -----
-        answer:
+        answer: the launch angle.
+
     """
     if ve_v0 < 0 or alpha < 0:
         raise ValueError(f"You have an invalid ratio or alpha value.")
-    # implementation of equations 17
-    sin_phi = ((1 + alpha) * (np.sqrt(1 - (alpha / (1 + alpha)) * ve_v0**2)))  # eq 17 step 1
+    # This is the implemenation of equation 17 from the lab manual.
+    sin_phi = ((1 + alpha) * (np.sqrt(1 - (alpha / (1 + alpha)) * ve_v0**2)))
+    # Using the arcsin(x) function to compute the arcsin of sin_phi.
     answer = arcsin(sin_phi)
     return answer
 
@@ -73,6 +66,8 @@ def launch_angle_range(ve_v0, alpha, tol_alpha):
     """
     Description of function:
     -----
+    This function computes the minimum and maximum launch angle within the tolerance for maximum altitude, and outputs
+     a 2-number array.
 
     Parameter(s):
     -----
@@ -82,16 +77,20 @@ def launch_angle_range(ve_v0, alpha, tol_alpha):
 
     Return(s):
     -----
-        phi_range:
+        phi_range: the 2 component array of the maximum and minimum launch angle.
+
     """
+
+    # Creating an empty list to store the array values.
     array_list = []
 
-    one_plus = ((1 + tol_alpha) * alpha)  # minimum allowable launch angle at max altitude (new alpha)
+    one_plus = ((1 + tol_alpha) * alpha)  # This will compute the minimum allowable launch angle at max altitude.
     array_list.append(launch_angle(ve_v0, one_plus))
 
-    one_minus = ((1 - tol_alpha) * alpha)  # maximum allowable launch angle at max altitude (new alpha)
+    one_minus = ((1 - tol_alpha) * alpha)  # This will compute the maximum allowable launch angle at max altitude.
     array_list.append(launch_angle(ve_v0, one_minus))
 
+    # Creating the array using the list.
     phi_range = np.array(array_list)
 
     return phi_range
