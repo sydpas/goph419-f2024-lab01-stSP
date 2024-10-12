3e9cc3f88ed9c605cf7ea28466d811c8ca65fdbe
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-10-12 12:44:13 -0600

    Edited the description of the function as well as all the comments

--- tests/tests.py
+++ tests/tests.py
@@ -11,32 +11,31 @@ def astest():
     """
     Description of function:
     -----
+    This function tests and compares the expected NumPy arcsin results vs. the actual results of the arcsin function
+    created by this user.
 
     Parameter(s):
     -----
-        tol: error tolerance.
-        x: ***
+        None.
 
     Return(s):
     -----
+        None.
 
     """
 
-    tol = 1.0e-8
-    x = 0.5  # Please input an x between -1 and 1 (inclusive).
+    tol = 1.0e-8  # Please enter the significant digit tolerance. The general tolerance is 1.0e-8.
+    x = 0.5  # Please input an x value between -1 and 1 (inclusive) to generate results that do not raise a ValueError.
 
-    arcsin(x)
-
-    # what is this test doing
     print(f"Testing arcsin({x})...")
 
-    # expected output
+    # Expected output.
     expected = np.asin(x)
 
-    # actual output
+    # Actual output.
     actual = arcsin(x)
 
-    # compare results
+    # Compare results.
     if np.abs((actual - expected) / expected) < tol:
         print(f"Passed, the expected value matches the actual value of {actual}!")
     else:
@@ -47,32 +46,33 @@ def angletest():
     """
     Description of function:
     -----
+    This function tests and compares the expected results of the launch angle function vs. the actual computed results
+    in the function created by the user.
 
     Parameter(s):
     -----
-        tol: error tolerance.
-        x: ***
+        None.
 
     Return(s):
     -----
+        None.
 
     """
 
-    tol = 1.0e-8
+    tol = 1.0e-8  # Please enter the significant digit tolerance. The general tolerance is 1.0e-8.
     ve_v0 = 2.0  # Please enter a ratio value.
     alpha = 0.3  # Please enter an alpha value.
 
-    # what is this test doing
     print(f"Testing launch_angle with a ve_v0 of ({ve_v0}) and an alpha of ({alpha})...")
 
-    # expected output
+    # Expected output.
     sin_phi = ((1 + alpha) * (np.sqrt(1 - (alpha / (1 + alpha)) * ve_v0 ** 2)))  # eq 17 step 1
     expected = np.asin(sin_phi)
 
-    # actual output
+    # Actual output.
     actual = launch_angle(ve_v0, alpha)
 
-    # compare results
+    # Compare results.
     if np.abs((actual - expected) / expected) < tol:
         print(f"Passed, the expected value matches the actual value of {actual}!")
     else:
@@ -83,12 +83,16 @@ def rangetest():
     """
     Description of function:
     -----
+    This function tests and compares the expected results of the launch angle range function vs. the actual computed
+    results in the function created by the user.
 
     Parameter(s):
     -----
+        None.
 
     Return(s):
     -----
+        None.
 
     """
 
@@ -96,10 +100,9 @@ def rangetest():
     alpha = 0.25  # Please enter an alpha value.
     tol_alpha = 0.04  # Please enter the alpha tolerance.
 
-    # what is this test doing
     print(f"Testing launch_angle_range with an alpha of ({alpha}) and a tol_alpha of ({tol_alpha})...")
 
-    # expected output
+    # Expected output.
     expected_list = []
     one_plus = ((1 + tol_alpha) * alpha)
     expected_list.append(launch_angle(ve_v0, one_plus))
@@ -107,10 +110,10 @@ def rangetest():
     expected_list.append(launch_angle(ve_v0, one_minus))
     expected_range = np.array(expected_list)
 
-    # actual output
+    # Actual output.
     actual_range = np.array(launch_angle_range(ve_v0, alpha, tol_alpha))
 
-    # compare results
+    # Compare results.
     if np.array_equal(actual_range, expected_range):
         print(f"Passed, the expected range matches the actual range of {actual_range}!")
     else:
